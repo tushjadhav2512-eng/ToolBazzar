eaa9d85059a4ccf4c96f5ae1f1a811c6af48cf17
tushjadhav2512-eng <tushjadhav2512@gmail.com>
2026-05-28 14:14:54 +0530

    Delete app directory

--- app/robots.js
+++
@@ -1,7 +0,0 @@
-export default function robots() {
-  const base = process.env.NEXT_PUBLIC_BASE_URL || 'https://toolbazaar.app'
-  return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: `${base}/sitemap.xml`
-  }
-}
